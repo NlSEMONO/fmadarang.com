925c914803c90ae5c39ebda0efa1642ea3176bf7
NlSEMONO <fmadarang2542@hotmail.com>
2023-10-22 18:57:00 -0400

    Added product data retrieval to backendof store app

--- main/sinnoh_stores/urls.py
+++ main/sinnoh_stores/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('get-locations', views.shop_locations)
+    path('get-locations', views.shop_locations),
+    path('get-products', views.products_and_locations),
 ]
--- main/sinnoh_stores/views.py
+++ main/sinnoh_stores/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import ShopLocation
+from .models import *
 
 # Create your views here.
 def shop_locations(request):
@@ -17,3 +17,26 @@ def shop_locations(request):
         shop_data.append(dict_so_far)
 
     return JsonResponse(shop_data, safe=False)
+
+def products_and_locations(request):
+    """
+    Returns all products information and locations they can be found in at stores in Sinnoh as JSON.
+    """
+    products = []
+    product_listings = ProductLister.objects.all()
+    shops = ShopLocation.objects.all()
+    for prod in product_listings:
+        locations = []
+        for shop in shops: 
+            if (len(shop.products.filter(name=prod.name)) > 0):
+                locations.append(shop.name)
+        print(prod)
+        products.append({
+            'id': prod.id,
+            'name': prod.name,
+            'price': prod.price,
+            'locations': locations,
+            'image': f'/Dream_{"_".join(prod.name.split(" "))}_Sprite.png'
+        })
+    
+    return JsonResponse(products, safe=False)
